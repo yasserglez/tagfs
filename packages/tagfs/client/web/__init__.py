2cb2957556e7eca639a0750e17a815d2fc1d0cc5
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-04-09 11:38:33 -0400

    Añadiendo requerimientos para el cliente web con Django.

---
+++ packages/tagfs/client/web/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Implementación de un cliente TagFS web.
+
+Este cliente web está implementado utilizando el framework de desarrollo
+web Django. La documentación de este paquete Python no se incluye en
+el documento generado automáticamente con la documentación del API.
+"""
