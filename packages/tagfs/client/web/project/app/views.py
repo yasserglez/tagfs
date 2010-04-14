325c9ddcb56ca7b8cffb58bbfb9b2aa7b3967602
Abel Puentes Luberta <abelpuentes@gmail.com>
2010-04-13 20:37:32 -0500

    Agregando los primeros templates

--- packages/tagfs/client/web/project/app/views.py
+++ packages/tagfs/client/web/project/app/views.py
@@ -6,9 +6,18 @@ from django.conf.settings import TAGFSCLIENT as client
 
 from forms import UploadForm
 
-UPLOAD_TEMPLATE = ''
+HOME_TEMPLATE = 'site/home.html'
+
+def home(request):
+	return render_to_response(HOME_TEMPLATE, 
+								context_instance=RequestContext(request))
+
 
 def upload_file(request):
+	"""
+	Se ocupa de poner un fichero en tagfs, 
+	verificando que se cumplen todos los requisitos del form.
+	"""
     form = UploadForm()
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
@@ -20,10 +29,10 @@ def upload_file(request):
             replication = form.cleaned_data['replication']
             save = client.put(name, description, tags, data, replication)
             form = UploadForm()
-            return render_to_response(UPLOAD_TEMPLATE, 
+            return render_to_response(HOME_TEMPLATE, 
                                         {'form_upload': form, 'save': save},
                                         context_instance=RequestContext(request))
     else:        
-        return render_to_response(UPLOAD_TEMPLATE, 
+        return render_to_response(HOME_TEMPLATE, 
                                     {'form_upload': form},
                                     context_instance=RequestContext(request))
