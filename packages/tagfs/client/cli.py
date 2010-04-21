398e93cc1b31ed25d83ef1d8ca855b829f622db8
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-04-21 01:28:40 -0400

    Implementando esqueleto para los comandos del cliente de consola.

--- packages/tagfs/client/cli.py
+++ packages/tagfs/client/cli.py
@@ -4,14 +4,24 @@
 Implementación de un cliente TagFS para la línea de comandos.
 """
 
-import time
+import textwrap
+
+try:
+    # Used by raw_input for history, if available.
+    import readline
+except ImportError:
+    # Ignore this exception on Windows platforms.
+    pass
 
 from tagfs.client import TagFSClient
+from tagfs import __version__
 
 
 class CLITagFSClient(TagFSClient):
     """
-    Client TagFS para línea de comandos.
+    Client TagFS para línea de comandos. 
+    
+    Este cliente emula el funcionamiento de un sistema de ficheros tradicional.
     """
 
     def __init__(self, address, replication):
@@ -28,19 +38,29 @@ class CLITagFSClient(TagFSClient):
         """
         super(CLITagFSClient, self).__init__(address)
         self._replication = replication
+        self._continue = True
+        self._cwd = '/'
         
     def start(self):
         """
         Inicializa el ciclo principal del cliente TagFS para la línea de comandos.
         """
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            # Ignore CTRL-C so it can be used to stop the client.
-            pass
-        finally:
-            self.terminate()
+        welcome = 'Welcome to TagFS v{version}.\n' 
+        print welcome.format(version=__version__)
+        while self._continue:
+            try:
+                prompt = 'TagFS:{cwd}> '.format(cwd=self._cwd)
+                user_input = raw_input(prompt)
+                cmd = user_input.split()[0]
+                args = user_input[len(cmd):].lstrip()
+                try:
+                    func = getattr(self, '_command_{cmd}'.format(cmd=cmd))
+                    func(args)
+                except AttributeError:
+                    print '\nInvalid command "{cmd}".\n'.format(cmd=cmd)
+            except (EOFError, KeyboardInterrupt):
+                print 'exit'
+                self._command_exit('')
             
     def put(self, name, description, tags, data):
         """
@@ -50,3 +70,34 @@ class CLITagFSClient(TagFSClient):
         """
         return super(CLITagFSClient, self).put(name, description, tags, data, 
                                                self._replication)
+
+    def _command_exit(self, args):
+        """
+        Usage: exit
+        
+        Ends the execution of the client.
+        """
+        self.terminate()
+        self._continue = False
+        
+    def _command_help(self, args):
+        """
+        Usage: help [command]
+        
+        Prints information about the usage of a command. If it is executed
+        without a command as argument it prints the available cmds.
+        """
+        if not args:
+            cmds = []
+            for attr in dir(self):
+                if attr.startswith('_command_'):
+                    cmds.append(attr[len('_command_'):])
+            single_line = 'Available commands: {cmds}.'.format(cmds=', '.join(cmds))
+            multiple_lines = textwrap.wrap(single_line)
+            print '\n{msg}\n'.format(msg='\n'.join(multiple_lines))
+        else:
+            try:
+                func = getattr(self, '_command_{cmd}'.format(cmd=args))
+                print textwrap.dedent(func.__doc__)
+            except AttributeError:
+                print '\nInvalid command "{cmd}".\n'.format(cmd=args)
