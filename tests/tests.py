a107637f779d020144a46b32e6848292983f1c28
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-05-05 00:18:05 -0400

    Completando implementación del framework de pruebas.

--- tests/tests.py
+++ tests/tests.py
@@ -5,79 +5,90 @@ Pruebas por unidad del Sistema.
 """
 
 import os
+import sys
+import time
 import unittest
 import threading
+import shutil
 
-from tagfs.server import TagFSServer
+# Add to the Python path the directory containing the packages in the source distribution. 
+SRC_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
+PACKAGES_DIR = os.path.abspath(os.path.join(SRC_DIR, 'packages'))
+sys.path.insert(0, PACKAGES_DIR)
+
+# Add the contrib directory to the Python path.
+CONTRIB_DIR = os.path.abspath(os.path.join(PACKAGES_DIR, 'tagfs', 'contrib'))
+sys.path.insert(0, CONTRIB_DIR)
+
 from tagfs.client import TagFSClient
 
 
-SRC_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
-
 TESTS_DIR = os.path.abspath(os.path.join(SRC_DIR, 'tests'))
 
-DATA_DIR = os.path.abspath(os.path.join(TESTS_DIR, 'data'))
+CLIENT_IP = '127.0.0.1'
 
-SERVER_DATA_DIR = os.path.abspath(os.path.join(DATA_DIR, 'server'))
+CLIENT_COUNT = 5
 
-CLIENT_DATA_DIR = os.path.abspath(os.path.join(DATA_DIR, 'client'))
-
-SERVER_INTERFACE = "127.0.0.1"
-
-CLIENT_INTERFACE = "127.0.0.1"
-
-SERVER_COUNT = 5
-
-SERVER_CAPACITY = 1073741824
-
-FILES_COUNT = 10
-
-MAX_FILE_CAPACITY = 1073741
+CLIENT_CAPACITY = 1024 * 1024 * 1024
 
 
-class Test(unittest.TestCase):
+class TagFS(unittest.TestCase):
     """
-    Pruebas por unidad del Sistema.   
+    Pruebas por unidades de TagFS.   
     """
 
     def setUp(self):
         """
-        Inicializa un ambiente para realizar las pruebas.
+        Inicializa el ambiente para realizar las pruebas.
         """
-        servers = []
-        threads = []
-        clients = []
-        path = os.path.abspath(os.path.join(SERVER_DATA_DIR, "server_{number}"))
-        for i in xrange(SERVER_COUNT):
-            # Starting the servers.
-            server_data = path.format(number = i)
-            os.mkdir(path)
-            server = TagFSServer(SERVER_INTERFACE, server_data, SERVER_CAPACITY)
-            servers.append(server)
-            # Starting the threads.
-            thread = threading.Thread(server.start)
-            thread.start()
-            threads.append(thread)
-        # Starting the client.
-        client = TagFSClient(CLIENT_INTERFACE)
-        self._servers = servers
-        self._threads = threads
-        self._client = client
-        
+        self._clients = []
+        clients_lock = threading.Lock()
+        self._client_threads = []
+        self._client_data_dirs = []
+        client_data_dir_template = os.path.join(TESTS_DIR, 'client{number}')
+        for index in xrange(CLIENT_COUNT):
+            # Creating the client.
+            client_data_dir = client_data_dir_template.format(number=index+1)
+            self._client_data_dirs.append(client_data_dir)
+            if os.path.isdir(client_data_dir):
+                shutil.rmtree(client_data_dir)
+            os.mkdir(client_data_dir)
+            def create_client():
+                client = TagFSClient(CLIENT_IP, client_data_dir, CLIENT_CAPACITY)
+                with clients_lock:
+                    self._clients.append(client)
+            client_thread = threading.Thread(target=create_client)
+            self._client_threads.append(client_thread)
+            # Wait for the clients to start.
+            clients_before = len(self._clients)
+            clients_after = clients_before
+            client_thread.start()
+            while clients_before == clients_after:
+                time.sleep(0.1)
+                with clients_lock:
+                    clients_after = len(self._clients)
+            
     def tearDown(self):
         """
         Termina el ambiente creado para realizar las pruebas. 
         """
-        for server in self._servers:
-            server.stop()
-        for thread in self._threads:
-            thread.join()
-        self._client.terminate()
+        for client in self._clients:
+            client.terminate()
+        for client_thread in self._client_threads:
+            client_thread.join()
+        for client_data_dir in self._client_data_dirs:
+            shutil.rmtree(client_data_dir)
 
-    def testName(self):
+    def test_a(self):
         pass
-
+    
+    def test_b(self):
+        pass
+    
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    module = os.path.basename(__file__)[:-3]
+    suite = unittest.TestLoader().loadTestsFromName(module)
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(suite)
+    sys.exit(not result.wasSuccessful())
