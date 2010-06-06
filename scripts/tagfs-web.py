e49d681f91a7ef8d4d395e12d25836f73a52effe
Andy Venet Pompa <andy.venet@gmail.com>
2010-06-06 12:50:28 -0500

    Modificado script para correr el server de Django

--- scripts/tagfs-web.py
+++ scripts/tagfs-web.py
@@ -2,47 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import time
 import socket
+import webbrowser
 import subprocess
 
 
 def get_usable_addrport(host='localhost', port_start=1024, port_end=49151):
     sock = socket.socket()
     addrport = None
-    for port in range(port_start, port_end + 1):
+    for port in xrange(port_start, port_end + 1):
         address = (host, port)
         try:
             sock.bind(address)
-        except socket.error, error:
-            if error.errno == 98:
-                pass
-        else:
-            addrport = '%s:%s' % (host, port)
+            addrport = port
+            break
+        except socket.error:
+            pass
     sock.close()
     return addrport
 
 
 def open_browser(root_url):
     try:
-        browser_args = ('/usr/bin/xdg-open', root_url)
-        subprocess.Popen(browser_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		print 'Opening Canipedia using your default web browser.'
+        webbrowser.open(root_url)
+        print 'Opening TagFs using your default web browser.'
     except OSError:
-        print 'Could not open Canipedia using your default web browser.'
+        print 'Could not open TagFs using your default web browser.'
         print 'Please, manually visit "%s",' % root_url
 
 def main():
     # Run the Django development server.
     addrport = get_usable_addrport(port_start=8000)
-    manage_script = os.path.join(os.path.dirname(__file__), os.pardir, 'django/project/manage.py')
-    server_args = ('/usr/bin/python', os.path.abspath(manage_script), 'runserver', addrport)
+    manage_script = os.path.join(os.path.dirname(__file__), os.pardir, 'packages/tagfs/client/web/project/manage.py')
+    server_args = (sys.executable, os.path.abspath(manage_script), 'runserver', str(addrport))
+    print server_args
     server_process = subprocess.Popen(server_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print 'Canipedia is running. You can stop Canipedia using CONTROL-C.'
+    print 'TagFs is running. You can stop TagFs using CONTROL-C.'
     # Wait for the Django server to start. Sleep 1 second.
     time.sleep(1)
     # Open the web browser.
-    root_url = 'http://%s/' % addrport
+    root_url = 'http://localhost:%s/' % addrport
     open_browser(root_url)
     # Wait until the Django server exits.
     try:
